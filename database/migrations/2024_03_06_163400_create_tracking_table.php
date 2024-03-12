feba438a8d04455841580f158cb36e32ea71c6f3
yousef badr <yousefbadr10@gmail.com>
2024-03-12 15:55:25 +0200

    Edit migration

--- database/migrations/2024_03_06_163400_create_tracking_table.php
+++ database/migrations/2024_03_06_163400_create_tracking_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('tracking', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('employees')->onDelete('cascade');
             $table->unsignedBigInteger('prod_id');
             $table->unsignedBigInteger('url_id');
             $table->timestamps();
